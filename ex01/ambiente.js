b5c40fa4ef6d31c5a9b920e4d0fbe12c796b397c
leocapra <leoccapra@gmail.com>
2022-08-23 18:30:05 -0300

    cont

--- ex01/ambiente.js
+++ ex01/ambiente.js
@@ -2,5 +2,11 @@ let clicar = document.querySelector('button#enviar')
 clicar.addEventListener('click', clicou)
 
 function clicou() {
-    window.alert('teste')
+    let numero = document.querySelector('input#num')
+    let num = Number(numero.value)
+    if (num === 0) {
+        window.alert('[Erro] Por favor, digite um número!')
+    } else {
+        num
+    }
 }
