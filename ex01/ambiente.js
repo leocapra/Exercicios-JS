380b88bb58dace8fdc920d3e16ee122bc8ac37fe
leocapra <leoccapra@gmail.com>
2022-08-23 19:11:11 -0300

    tabuada concluida

--- ex01/ambiente.js
+++ ex01/ambiente.js
@@ -4,9 +4,19 @@ clicar.addEventListener('click', clicou)
 function clicou() {
     let numero = document.querySelector('input#num')
     let num = Number(numero.value)
+    let divisao = document.querySelector('div.res')
+    let divisao01 = document.querySelector('div.res01')
+    let divisao02 = document.querySelector('div.res02')
+    divisao.innerHTML = ``
     if (num === 0) {
         window.alert('[Erro] Por favor, digite um número!')
     } else {
-        num
+        for (let um = 1; um <= 5; um++) {
+            divisao01.innerHTML += `<p>${num} x ${um} = ${num*um} </p>`
+        }
+
+        for (let dois = 6; dois <= 10; dois++) {
+            divisao02.innerHTML += `<p>${num} x ${dois} = ${num*dois} </p>`
+        }
     }
 }
